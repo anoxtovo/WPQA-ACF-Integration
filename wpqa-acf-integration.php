d629e6419ab88f91f57e379d88f41af2781dd2b4
Thumula Basura Suraweera <59816597+anoxtovo@users.noreply.github.com>
2024-08-26 10:52:25 +0530

    fix input boxes not showing

--- wpqa-acf-integration.php
+++ wpqa-acf-integration.php
@@ -2,44 +2,48 @@
 /*
 Plugin Name: WPQA ACF Integration
 Description: Integrates WPQA plugin with ACF to add custom fields to the question form.
-Version: 1.1
+Version: 1.2
 Author: Thumula Basura Suraweera
 Author URI: https://www.thumulabasura.com/wpqa-acf-integration
 License: GPLv2
 */
 
-// Hook into the WPQA form display to inject ACF fields
-function wpqa_acf_inject_custom_fields($out, $type, $question_add, $question_edit, $get_question) {
+// Hook into the WPQA form display to inject ACF fields after offer_title
+function wpqa_acf_inject_custom_fields($out, $question_sort_option, $question_sort, $sort_key, $sort_value) {
     // Ensure that we are working with the 'question' post type
-    if (get_post_type($get_question) != 'question') {
+    global $post;
+    if (get_post_type($post) != 'question') {
         return $out;
     }
 
-    if (function_exists('get_field')) {
-        $offer_url = get_field('offer_url', $get_question);
-        $start_time = get_field('start_time', $get_question);
-        $expire_time = get_field('expire_time', $get_question);
+    // Inject custom fields only after offer_title
+    if ($sort_key == "offer_title") {
+        if (function_exists('get_field')) {
+            $offer_url = get_field('offer_url', $post->ID);
+            $start_time = get_field('start_time', $post->ID);
+            $expire_time = get_field('expire_time', $post->ID);
 
-        // Display Offer URL field
-        $out .= '<div class="wpqa_offer_url">
-            <label for="acf_offer_url">'.esc_html__("Offer URL", "wpqa").'<span class="required">*</span></label>
-            <input type="url" name="acf_offer_url" id="acf_offer_url" class="form-control" value="'.esc_attr($offer_url).'">
-            <span class="form-description">'.esc_html__("Please enter an offer URL for the question.", "wpqa").'</span>
-        </div>';
+            // Display Offer URL field
+            $out .= '<div class="wpqa_offer_url">
+                <label for="acf_offer_url">'.esc_html__("Offer URL", "wpqa").'<span class="required">*</span></label>
+                <input type="url" name="acf_offer_url" id="acf_offer_url" class="form-control" value="'.esc_attr($offer_url).'">
+                <span class="form-description">'.esc_html__("Please enter an offer URL for the question.", "wpqa").'</span>
+            </div>';
 
-        // Display Start Time field
-        $out .= '<div class="wpqa_start_time">
-            <label for="acf_start_time">'.esc_html__("Start Time", "wpqa").'<span class="required">*</span></label>
-            <input type="datetime-local" name="acf_start_time" id="acf_start_time" class="form-control" value="'.esc_attr($start_time).'">
-            <span class="form-description">'.esc_html__("Please select a start time for the question.", "wpqa").'</span>
-        </div>';
+            // Display Start Time field
+            $out .= '<div class="wpqa_start_time">
+                <label for="acf_start_time">'.esc_html__("Start Time", "wpqa").'<span class="required">*</span></label>
+                <input type="datetime-local" name="acf_start_time" id="acf_start_time" class="form-control" value="'.esc_attr($start_time).'">
+                <span class="form-description">'.esc_html__("Please select a start time for the question.", "wpqa").'</span>
+            </div>';
 
-        // Display Expire Time field
-        $out .= '<div class="wpqa_expire_time">
-            <label for="acf_expire_time">'.esc_html__("Expire Time", "wpqa").'<span class="required">*</span></label>
-            <input type="datetime-local" name="acf_expire_time" id="acf_expire_time" class="form-control" value="'.esc_attr($expire_time).'">
-            <span class="form-description">'.esc_html__("Please select an expire time for the question.", "wpqa").'</span>
-        </div>';
+            // Display Expire Time field
+            $out .= '<div class="wpqa_expire_time">
+                <label for="acf_expire_time">'.esc_html__("Expire Time", "wpqa").'<span class="required">*</span></label>
+                <input type="datetime-local" name="acf_expire_time" id="acf_expire_time" class="form-control" value="'.esc_attr($expire_time).'">
+                <span class="form-description">'.esc_html__("Please select an expire time for the question.", "wpqa").'</span>
+            </div>';
+        }
     }
 
     return $out;
